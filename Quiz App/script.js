cb1169f09a0bf371f9441c2cb48ec4ed39c356fc
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2024-08-29 00:04:56 +0530

    Add files via upload

---
+++ Quiz App/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question : "Which is largest animal in the world?",
+        answers : [
+            {  type:"Shark", correct:false } ,
+            {  type:"Blue whale", correct:true } ,
+            {  type:"Elephant", correct:false } ,
+            {  type:"Giraffe", correct:false } ,
+        ]
+    },
+    {
+        question : "Which is largest animal in the world?",
+        answers : [
+            {  type:"Shark", correct:false } ,
+            {  type:"Blue whale", correct:true } ,
+            {  type:"Elephant", correct:false } ,
+            {  type:"Giraffe", correct:false } ,
+        ]
+    },
+    {
+        question : "Which is largest animal in the world?",
+        answers : [
+            {  type:"Shark", correct:false } ,
+            {  type:"Blue whale", correct:true } ,
+            {  type:"Elephant", correct:false } ,
+            {  type:"Giraffe", correct:false } ,
+        ]
+    },
+    {
+        question : "Which is largest animal in the world?",
+        answers : [
+            {  type:"Shark", correct:false } ,
+            {  type:"Blue whale", correct:true } ,
+            {  type:"Elephant", correct:false } ,
+            {  type:"Giraffe", correct:false } ,
+        ]
+    }
+];
